0e8c70f9be8729c1bdceb0e0cd391b14d44afceb
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-04-03 02:26:55 +0900

    Use memo.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -12,13 +12,22 @@ export default function App() {
   const onClickOpen = () => {
     setOpen(!open);
   };
+  //関数をpropsとして渡す場合はuseCallbackを使って関数のメモ化が必要
+  //でないと、都度関数が再生成されてしまい再レンダリングが起きる
+  //配列に入れた値を監視する
+  const onClickClose = useCallback(() => {
+    setOpen(!open);
+  }, [setOpen]);
+  //再レンダリングの度に計算しないようにする。（空の配列の場合はマウント時のみ計算してそのあと4を使いまわす）
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea onClickClose={onClickClose} open={open} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -8,20 +8,21 @@ const style = {
 
 //memoで囲うことでpropsを変更しない限り際レンダリングしなくなる
 //今後肥大化が予想されるコンポーネントは全てmemoで囲う
-export const ChildArea = memo(({ open }) => {
+export const ChildArea = memo(({ open, onClickClose }) => {
   console.log("ChildAreaがレンダリングされた！");
   //2000件のデータを用意
   const data = [...Array(2000).keys()];
 
-  data.forEach((element) => {
-    console.log("...");
-  });
+  // data.forEach((element) => {
+  //   console.log("...");
+  // });
 
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
